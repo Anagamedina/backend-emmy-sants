bf1e1a3d08694de17dffb584f13230e05a38f2bc
avdbp <alejandro.vdb@gmail.com>
2023-09-20 16:58:40 +0200

    comenzando admin.route con el ProductCreate

--- models/User.model.js
+++ models/User.model.js
@@ -1,6 +1,29 @@
-const {, model } = require("mongoose");
-const { userSchema } = require("./userSchema");
+const { Schema, model } = require("mongoose");
 
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required."],
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 
